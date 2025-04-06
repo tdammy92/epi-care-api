b79b5bb87bbd9f2dfed388e595f34c12dcf4e763
Taiwo Damilola <tdammy92@gmail.com>
2025-04-06 19:08:25 +0100

    update user handler

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,11 +2,12 @@ import { NOT_FOUND, OK } from "../constants/statusCode";
 import UserModel from "../models/user.model";
 import appAssert from "../utils/appAssert";
 import catchErrors from "../utils/catchErrors";
+import { getMenuLinksForRole } from "../utils/getRoles";
 
 export const getUserHandler = catchErrors(async (req, res) => {
   //@ts-ignore
   const user = await UserModel.findById(req.userId);
   appAssert(user, NOT_FOUND, "User not found");
-  // return res.status(OK).json(user.omitPassword());
-  return res.status(OK).json(user.omitSensitive());
+  const userData = {...user.omitSensitive(),access:getMenuLinksForRole(user.role)};
+  return res.status(OK).json(userData);
 });
